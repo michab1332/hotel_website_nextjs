ececc92f3892b683ce281f03d41bbd581fd0a3db
Michal Bonowicz <bonowiczmichal1332@gmail.com>
2023-03-21 22:58:31 +0100

    work on service section

--- pages-components/home-page/index.tsx
+++ pages-components/home-page/index.tsx
@@ -2,6 +2,7 @@ import AboutSection from "./AboutSection";
 import HeroSection from "./HeroSection";
 import GallerySection from "./GallerySection";
 import ContentSection from "@/shared-components/ContentSection";
+import ServicesSection from "./ServicesSection";
 
 import ContentSectionImage from "@/public/hotel_room.jpg";
 
@@ -13,6 +14,7 @@ const HomePage = () => {
             <GallerySection />
             <ContentSection img={ContentSectionImage} title="Lorem Ipsum" content="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English." />
             <ContentSection reverse img={ContentSectionImage} title="Lorem Ipsum" content="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English." />
+            <ServicesSection />
         </>
     )
 }
